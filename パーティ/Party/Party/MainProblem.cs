ce78dcaa916ebdf68eca8b6246d87e84e9371db0
fmy02s <fmy.02s@gmail.com>
2022-05-04 10:11:18 +0900

    パーティープログラム新規作成

---
+++ パーティ/Party/Party/MainProblem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Party
+{
+	class MainProblem
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
