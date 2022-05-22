193f1fbad1efb84714df9e6b6f0071b32baa1cc7
fmy02s <fmy.02s@gmail.com>
2022-05-22 18:41:27 +0900

    FriendManagerクラスの作成。 ＋テストコード作成。（GetInviteCountのみ）

---
+++ パーティ/Party/ClassLibrary/FriendManager.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+	public class FriendManager
+	{
+
+		public FriendManager(int studentCount)
+		{
+			_students = new bool[studentCount, studentCount] ;
+			_studentCount = studentCount;
+		}
+
+		public void SetFriendshipInfo(int studentNo1, int studentNo2)
+		{
+			// indexは0から始まるため-1を行う
+			_students[studentNo1 - 1, studentNo2 - 1] = true;
+			_students[studentNo2 - 1, studentNo1 - 1] = true;
+		}
+
+		public int GetInviteCount(int studentNo)
+		{
+			int inviteCount = 0;
+			var addInvitationNo = new List<int>();
+			for (int i = 0; i < _studentCount; i++)
+			{
+				if (!_students[studentNo - 1, i])
+					continue;
+
+				inviteCount++;
+
+				//友達の友達のチェック
+				for (int j = 0; j < _studentCount; j++)
+				{
+					if (!_students[i, j])
+						continue;
+
+					//既に友達もしくは自身かの確認
+					if (_students[studentNo - 1, j] || (studentNo - 1 == j))
+						continue;
+
+					if (!addInvitationNo.Contains(j))
+						addInvitationNo.Add(j);
+				}
+			}
+			inviteCount += addInvitationNo.Count;
+			return inviteCount;
+		}
+
+		private bool[,] _students;
+		private int _studentCount;
+	}
+}
---
+++ パーティ/Party/ClassLibraryTest/FriendManagerTest.cs
@@ -0,0 +1,48 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ClassLibraryTest
+{
+	[TestClass]
+	public class FriendManagerTest
+	{
+		[TestMethod]
+		public void GetInviteCountTest1()
+		{
+			var target = new FriendManager(3);
+			Assert.AreEqual(0, target.GetInviteCount(1));
+
+			target.SetFriendshipInfo(1, 2);
+			Assert.AreEqual(1, target.GetInviteCount(1));
+		}
+
+		// 友達の友達も招待するかどうか
+		[TestMethod]
+		public void GetInviteCountTest2()
+		{
+			var target = new FriendManager(3);
+			Assert.AreEqual(0, target.GetInviteCount(1));
+
+			target.SetFriendshipInfo(1, 2);
+			target.SetFriendshipInfo(2, 3);
+			Assert.AreEqual(2, target.GetInviteCount(1));
+		}
+
+		// 友達の友達も招待するかどうか
+		// 友達(2,4)の友達（3(共通の友達))重複しないか
+		[TestMethod]
+		public void GetInviteCountTest3()
+		{
+			var target = new FriendManager(4);
+			Assert.AreEqual(0, target.GetInviteCount(1));
+
+			target.SetFriendshipInfo(1, 2);
+			target.SetFriendshipInfo(1, 4);
+			target.SetFriendshipInfo(2, 3);
+			target.SetFriendshipInfo(3, 4);
+
+			Assert.AreEqual(3, target.GetInviteCount(1));
+		}
+	}
+}
